4220611a4d461fa3ba29e81735faea894821f20a
Wesley <88836392+WesleyDRC@users.noreply.github.com>
2025-01-21 23:36:05 -0300

    refactor: improving code

--- app/backend/src/modules/finances/useCases/CreateFinanceUseCase.ts
+++ app/backend/src/modules/finances/useCases/CreateFinanceUseCase.ts
@@ -117,62 +117,16 @@ export class CreateFinanceUseCase implements IUseCase {
 
     const [day, month, year] = startRental.split("/").map(Number);
 
-    const currentMonth = new Date().getMonth() + 1;
-    const currentYear = new Date().getFullYear();
-    const currentDay = new Date().getDay() + 1;
-
-    const startRentalDate = new Date(day, month, currentDay)
-    const currentDate = new Date(currentYear, currentMonth, currentDay)
-
-    console.log(`Current Date: ${currentDate}`)
-
-    const quantityMonthsPerYear = 12
-
+    const currentDate = new Date();
+    const startDate = new Date(year, month - 1, day); // (0-11) months in javascript go from 0 to 11
+    const endDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), day);
+  
     const months = [];
-
-    let quantityMonths = 0
-
-    let monthsRent = []
-
-    let firstAddition = true
-
-    console.log(`Year: ${year}`)
-    console.log(`current year: ${currentYear}`)
-
-    let yearStarted = year
-
-    while(yearStarted <= currentYear) {
-      if(firstAddition && yearStarted <= currentYear - 1)
-        for(let j = month; j <= quantityMonthsPerYear; j++) {
-          const monthCreated = j - 1;
-          const date = new Date(yearStarted, monthCreated, day);
-          monthsRent.push(date)
-        }
-        yearStarted += 1
-        firstAddition = false
-
-      if(yearStarted <= currentYear - 1) {
-        for(let k = 1; k <= quantityMonthsPerYear; k++) {
-          const monthCreated = k - 1;
-          const date = new Date(yearStarted, monthCreated, day);
-          monthsRent.push(date)
-        }
-        yearStarted += 1
-      }
-      else {
-        for (let i = 1; i <= currentMonth; i++) {
-          const monthCreated = i - 1;
-          const date = new Date(currentYear, monthCreated, day);
-
-          monthsRent.push(date)
-        }
-      }
-    }
-
-    for (let i = 0; i < monthsRent.length; i++) {
+  
+    for (let date = new Date(startDate); date <= endDate; date.setMonth(date.getMonth() + 1)) {
       months.push(
         await this.rentRepository.createRentMonth({
-          dateMonth: monthsRent[i],
+          dateMonth: new Date(date), 
           rentId: rentId,
         })
       );
